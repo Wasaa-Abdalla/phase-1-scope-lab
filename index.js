c59ce55acbc10892ea39d7f6831ea69090b75fb2
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-07-22 22:07:24 +0300

    Attempted

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(name) {
+    name = customerName;
+    console.log(name.toUpperCase());
+    
+}
+
+upperCaseCustomerName();
+
+function setBestCustomer(name){
+    var customerName = 'Jaffar';
+    name = customerName;
+    console.log(name);
+}
+setBestCustomer();
+
+function changeLeastFavoriteCustomer(customerName) {
+    console.log(customerName);
+    
+}
+changeLeastFavoriteCustomer();
